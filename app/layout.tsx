b03ac4a11badb0db1a5d9cefabf78e6f843aeab2
DESKTOP-P0CS25Q\PC <sstmddbs@naver.com>
2026-03-15 22:42:51 +0900

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,6 +119,19 @@ export default function RootLayout({
         />
         <Script
           async
+          src="https://www.googletagmanager.com/gtag/js?id=G-NKGCSCLZGS"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NKGCSCLZGS');
+          `}
+        </Script>
+        <Script
+          async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5378247298190063"
           crossOrigin="anonymous"
           strategy="afterInteractive"
